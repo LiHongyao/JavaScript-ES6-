7956f158d3f8bb6a1a51fb8adf9058fef43799d4
Li-HONGYAO <lihy_online@163.com>
2021-05-06 18:19:28 +0800

    内容更新

--- 源码/01. ES6/index.js
+++ 源码/01. ES6/index.js
@@ -1,28 +1,30 @@
 /*
  * @Author: Li-HONGYAO
  * @Date: 2021-04-30 14:50:16
- * @LastEditTime: 2021-04-30 18:11:39
+ * @LastEditTime: 2021-05-06 18:10:32
  * @LastEditors: Li-HONGYAO
  * @Description:
- * @FilePath: \源码\01. ES6\index.js
+ * @FilePath: \01. ES6\index.js
  */
 
-
-const getName = () => new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('Muzili');
-  }, 1000);
-})
-
-const getJob = () => new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('前端工程师');
-  }, 1000);
-})
-
-;(async () => {
-  console.log('程序执行')
-  const name = await getName();
-  const job  = await getJob();
-  console.log(`${name} - ${job}`);
-})(); 
+class Student {
+  constructor(name, age, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+  description() {
+    console.log(`
+        姓名：${this.name}
+        年龄：${this.age}
+        性别：${this.gender}
+    `);
+  }
+}
+// 创建类实例
+let stu = new Student(`Petter`, `26`, `male`);
+console.log(stu.hasOwnProperty(`name`)); // true
+console.log(stu.hasOwnProperty(`age`)); // true
+console.log(stu.hasOwnProperty(`gender`)); // true
+console.log(stu.hasOwnProperty(`description`)); // false
+console.log(stu.__proto__.hasOwnProperty(`description`)); // true
